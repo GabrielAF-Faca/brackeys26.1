6cadb0686d57eb7736031f0f90a682840b25a8d3
Your Name <you@example.com>
2026-02-21 12:53:43 -0300

    Screen shake adicionado

--- Scenes/room_zone_camera_2d.gd
+++ Scenes/room_zone_camera_2d.gd
@@ -4,17 +4,54 @@ class_name RoomZoneCamera2D
 var overlapping_zones: Array = []
 var active_zone: Area2D
 
+@export_category("Settings")
 @export var player_node: CharacterBody2D
+
+#@export_category("Screen Shake")
+var shake_intensity: float = 0.0
+var active_shake_time: float = 0.0
+
+var shake_decay: float = 5.0
+
+var shake_time: float = 0.0
+var shake_time_speed: float = 20.0
+
+var noise = FastNoiseLite.new()
 
 var follow_player: bool = false
 
 
 func _ready() -> void:
 	add_to_group("RoomZoneCamera")
+	Global.shake_screen.connect(screen_shake)
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if follow_player and player_node:
 		global_position = player_node.global_position
+	
+	if active_shake_time > 0:
+		shake_time += delta * shake_time_speed
+		active_shake_time -= delta
+		
+		offset = Vector2(
+			noise.get_noise_2d(shake_time, 0) * shake_intensity,
+			noise.get_noise_2d(0, shake_time) * shake_intensity
+		)
+		
+		shake_intensity = max(shake_intensity - shake_decay * delta, 0)
+	
+	else:
+		offset = lerp(offset, Vector2.ZERO, 10.5*delta)
+
+func screen_shake(intensity: int, time: float):
+	randomize()
+	noise.seed = randi()
+	noise.frequency = 2.0
+	
+	shake_intensity = intensity
+	active_shake_time = time
+	shake_time = 0.0
+
 
 func _process(_delta: float) -> void:
 	
